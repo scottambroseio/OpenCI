d60e329da9efc713f9784b1900c01a5a073f07a1
Scott Ranger <millenniumnight@googlemail.com>
2017-07-09 18:29:46 +0100

    IOC container project scaffolded

---
+++ OpenCI.IOC/UnityContainerFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Practices.Unity;
+
+namespace OpenCI.IOC
+{
+    public static class UnityContainerFactory
+    {
+        public static UnityContainer CreateContainer()
+        {
+            var container = new UnityContainer();
+
+            return container;
+        }
+    }
+}
