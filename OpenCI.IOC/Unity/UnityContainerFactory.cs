dfe5946aaade24344097a31a005698dcdc319121
Scott Ranger <millenniumnight@googlemail.com>
2017-07-22 09:48:21 +0100

    Updated IOC project to reflect identity changes

--- OpenCI.IOC/Unity/UnityContainerFactory.cs
+++ OpenCI.IOC/Unity/UnityContainerFactory.cs
@@ -1,16 +1,10 @@
-﻿using System.Web;
-using AutoMapper;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.Owin;
-using Microsoft.Owin.Security;
+﻿using AutoMapper;
 using Microsoft.Practices.Unity;
 using OpenCI.AutoMapper;
 using OpenCI.Business.Contracts;
 using OpenCI.Business.Implementation;
 using OpenCI.Data.Contracts;
 using OpenCI.Data.Implementation;
-using OpenCI.Identity.Dapper;
-using OpenCI.IOC.Identity;
 using ConnectionHelper = OpenCI.Data.Implementation.ConnectionHelper;
 using IConnectionHelper = OpenCI.Data.Contracts.IConnectionHelper;
 
@@ -30,19 +24,6 @@ namespace OpenCI.IOC.Unity
             // Business
             container.RegisterType<IProjectOperations, ProjectOperations>();
             container.RegisterType<IPlanOperations, PlanOperations>();
-
-            // Identity
-            container.RegisterType<OpenCI.Identity.Dapper.IConnectionHelper, OpenCI.Identity.Dapper.ConnectionHelper>(
-                new InjectionConstructor());
-            container.RegisterType<IUserStore<IdentityUser, int>, UserStore>();
-            container.RegisterType<IRoleStore<IdentityRole, int>, RoleStore>();
-            container.RegisterType<UserManager<IdentityUser, int>>(
-                new InjectionFactory(c => UserManagerFactory.Create(c.Resolve<IUserStore<IdentityUser, int>>())));
-            container.RegisterType<RoleManager<IdentityRole, int>>(
-                new InjectionFactory(c => RoleManagerFactory.Create(c.Resolve<IRoleStore<IdentityRole, int>>())));
-            container.RegisterType<SignInManager<IdentityUser, int>>();
-            container.RegisterType<IAuthenticationManager>(
-                new InjectionFactory(c => HttpContext.Current.GetOwinContext().Authentication));
 
             // Misc
             container.RegisterType<IMapper>(new InjectionFactory(c => AutoMapperFactory.CreateMapper()));
