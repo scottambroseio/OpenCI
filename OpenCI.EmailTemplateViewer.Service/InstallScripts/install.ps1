f15573aba98128fa4b5fe5fcd697cad4e779ff6b
Scott Ranger <millenniumnight@googlemail.com>
2017-08-10 21:24:23 +0100

    Email template viewer installation script

---
+++ OpenCI.EmailTemplateViewer.Service/InstallScripts/install.ps1
@@ -0,0 +1,12 @@
+﻿Param(
+	[Parameter(Mandatory=$true)]
+	[string]$BinPath
+);
+
+If (-Not (Test-Path $BinPath)) {
+	throw "$BinPath doesn't exist"
+}
+Else {
+	sc.exe create OpenCIEmailTemplateViewer binPath= %1
+	sc.exe start OpenCIEmailTemplateViewer
+}
